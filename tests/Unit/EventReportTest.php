f689449c50ddf4dbc01613edea04abfe590fc8ce
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-07-07 23:17:10 +0800

    done event report test cases

--- tests/Unit/EventReportTest.php
+++ tests/Unit/EventReportTest.php
@@ -18,7 +18,7 @@ class EventReportTest extends TestCase
      */
 
     use RefreshDatabase;
-/*
+
     public function test_creates_an_event_with_all_mandatory_fields_filled_out()
     {
         // TC-CREATE-AR-1
@@ -306,18 +306,122 @@ class EventReportTest extends TestCase
             'beneficiaries' => 'Sample Beneficiaries',
             'sponsors' => 'Sample Sponsors',
             'budget' => '1000',];
-        // Create Event Report
-        $this->actingAs($user)->call('POST', '/e', $test_data);
-        // Get Event Report URL slug
         $test_data_slug = Str::replace(' ', '-', $test_data['title']).'-'.Carbon::createFromFormat('Y-m-d', $test_data['date'])->format('Y');
+        // Create Event Report
+        $response1 = $this->actingAs($user)->call('POST', '/e', $test_data);
+        $response1->assertOk();
+        // Get Response
+        $response2 = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug);
+        $response2->assertOk();
+    }
+    public function test_view_an_event_with_unknown_url()
+    {
+        // TC-VIEW-AR-3
+        // Test User
+        $user = User::factory()->create();
+        // Test Data
+        $test_data_slug = 'unknown-event-2021';  
         
         // Get Response
         $response = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug);
-        $response->assertOk();
-    }*/
-    public function test_view_an_event_with_unknown_url()
+        $response->assertNotFound();
+    }
+    public function test_edit_an_event_with_known_url()
     {
-        // TC-VIEW-AR-2
+        // TC-EDIT-AR-2
+        // Test User
+        $user = User::factory()->create();
+        // Test Data
+        $test_data = [
+            '_token' => csrf_token(),
+            'title' => 'Sample Event',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'objective' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'date' => '2021-01-01',
+            'start_time' => '08:00',
+            'end_time' => '15:00',
+            'venue' => 'Sample Venue',
+            'activity_type' => 'Sample Activity Type',
+            'beneficiaries' => 'Sample Beneficiaries',
+            'sponsors' => 'Sample Sponsors',
+            'budget' => '1000',];
+        // Get Event Report URL slug
+        $test_data_slug = Str::replace(' ', '-', $test_data['title']).'-'.Carbon::createFromFormat('Y-m-d', $test_data['date'])->format('Y');
+        
+        // Create Event Report and Get Response
+        $response1 = $this->actingAs($user)->call('POST', '/e', $test_data);
+        $response1->assertOk();
+        
+        // Access /edit thru the created Event Report
+        $response2 = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug.'/edit');
+        $response2->assertOk();
+    }
+    public function test_edit_an_event_and_save()
+    {
+        // TC-EDIT-AR-3
+        // Test User
+        $user = User::factory()->create();
+        // Event Data to Edit
+        $data = [
+            '_token' => csrf_token(),
+            'title' => 'Sample Event',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'objective' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'date' => '2021-01-01',
+            'start_time' => '08:00',
+            'end_time' => '15:00',
+            'venue' => 'Sample Venue',
+            'activity_type' => 'Sample Activity Type',
+            'beneficiaries' => 'Sample Beneficiaries',
+            'sponsors' => 'Sample Sponsors',
+            'budget' => '1000',];
+        // Create Event Report
+        $this->actingAs($user)->call('POST', '/e', $data);
+        // Get Event Report URL slug
+        $data_slug = Str::replace(' ', '-', $data['title']).'-'.Carbon::createFromFormat('Y-m-d', $data['date'])->format('Y');
+        
+        // Test Data
+        $test_data = [
+            '_token' => csrf_token(),
+            'title' => 'Sample Event 2',
+            'description' => 'Lorem ipsum dolor',
+            'objective' => 'Lorem ipsum dolor',
+            'date' => '2020-12-31',
+            'start_time' => '08:30',
+            'end_time' => '15:30',
+            'venue' => 'Sample Venue 2',
+            'activity_type' => 'Sample Activity Type 2',
+            'beneficiaries' => 'Sample Beneficiaries 2',
+            'sponsors' => 'Sample Sponsors 2',
+            'budget' => '2000',];
+        $test_data_slug = Str::replace(' ', '-', $test_data['title']).'-'.Carbon::createFromFormat('Y-m-d', $test_data['date'])->format('Y');
+
+        // Update Event Report
+        $response1 = $this->actingAs($user)->call('PATCH', '/e/'.$data_slug, $test_data);
+        // Get Response
+        // Check if redirected to updated slug
+        $response1->assertRedirect('/e/'.$test_data_slug);
+        // Check if title field is updated
+        $this->assertEquals(Event::where('slug', $test_data_slug)->value('title'), $test_data['title']);
+        // Check if URL Slug is updated
+        $response2 = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug);
+        $response2->assertOk();
+    }
+    public function test_edit_an_event_with_invalid_url()
+    {
+        // TC-EDIT-AR-5
+        // Test User
+        $user = User::factory()->create();
+        // Test Data
+        $test_data_slug = 'unknown-event-2021';  
+        
+        // Get Response
+        $response = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug.'/edit');
+        $response->assertNotFound();
+    }
+    public function test_delete_an_event()
+    {
+        // TC-DELETE-AR-1
         // Test User
         $user = User::factory()->create();
         // Test Data
@@ -337,11 +441,43 @@ class EventReportTest extends TestCase
         // Create Event Report
         $this->actingAs($user)->call('POST', '/e', $test_data);
         // Get Event Report URL slug
-        $test_data_slug = 'unknown-event-2021';
-        
-        // Get Response
-        $response = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug);
-        $response->assertOk();
-    }
+        $test_data_slug = Str::replace(' ', '-', $test_data['title']).'-'.Carbon::createFromFormat('Y-m-d', $test_data['date'])->format('Y');
+        $test_csrf = ['_token' => csrf_token()];
 
+        // Get Response
+        // Delete Event and search for its unique slug in the database
+        $response = $this->actingAs($user)->call('DELETE', '/e/'.$test_data_slug, $test_csrf);
+        $response->assertRedirect('/e');
+        $response2 = $this->assertEquals(Event::where('slug', $test_data_slug)->doesntExist(), TRUE);
+    }
+    public function test_delete_an_event_and_try_to_access_it_with_known_url()
+    {
+        // TC-DELETE-AR-2
+        // Test User
+        $user = User::factory()->create();
+        // Test Data
+        $test_data = [
+            '_token' => csrf_token(),
+            'title' => 'Sample Event',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'objective' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            'date' => '2021-01-01',
+            'start_time' => '08:00',
+            'end_time' => '15:00',
+            'venue' => 'Sample Venue',
+            'activity_type' => 'Sample Activity Type',
+            'beneficiaries' => 'Sample Beneficiaries',
+            'sponsors' => 'Sample Sponsors',
+            'budget' => '1000',];
+        // Create Event Report
+        $this->actingAs($user)->call('POST', '/e', $test_data);
+        // Get Event Report URL slug
+        $test_data_slug = Str::replace(' ', '-', $test_data['title']).'-'.Carbon::createFromFormat('Y-m-d', $test_data['date'])->format('Y');
+        $test_csrf = ['_token' => csrf_token()];
+
+        // Delete Event and try to access it with its known URL
+        $this->actingAs($user)->call('DELETE', '/e/'.$test_data_slug, $test_csrf);
+        $response = $this->actingAs($user)->call('GET', '/e/'.$test_data_slug);
+        $response->assertNotFound();
+    }
 }
