ed6652bdc3e196bcad3d762d21a61451d4a52845
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-10-20 15:00:00 +0800

    calendar try

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Event;
+
+use Carbon\Carbon;
+use DateTime;
 
 use App\Http\Controllers\Controller as Controller;
 
@@ -27,7 +31,49 @@ class HomeController extends Controller
     public function index()
     {
         $loginAlert = $this->showLoginAlert();
-        return view('admin.home', compact('loginAlert'));
+        $calendarEvents = collect();
+        $allEvents = Event::all();
+        $calendarEventID = 1;
+        foreach ($allEvents as $event) 
+        {
+            if((new DateTime($event->start_date))->diff(new DateTime($event->end_date))->d > 0)
+            {
+                $eventLengthInDays = (new DateTime($event->start_date))->diff(new DateTime($event->end_date))->d + 1;
+
+                for ($i = 0; $i < $eventLengthInDays; $i++) 
+                { 
+                    $arr = array(
+                        'id' => $calendarEventID,
+                        'title' => $event->title,
+                        'allDay' => false,
+                        'start' => Carbon::parse(strtotime($event->start_date . $event->start_time))->addDays($i),
+                        'end' => Carbon::parse(strtotime($event->start_date . $event->end_time))->addDays($i),
+                        'url' => route('event.show', ['event_slug' => $event->slug]),
+                    );
+                    $calendarEvents->push($arr);
+                    $calendarEventID+=1;
+                }
+            }
+            else
+            {
+                $arr = array(
+                    'id' => $calendarEventID,
+                    'title' => $event->title,
+                    'allDay' => false,
+                    'start' => Carbon::parse(strtotime($event->start_date . $event->start_time)),
+                    'end' => Carbon::parse(strtotime($event->end_date . $event->end_time)),
+                    'url' => route('event.show', ['event_slug' => $event->slug]),
+                );
+               
+                $calendarEvents->push($arr);
+                $calendarEventID+=1;
+            }
+
+        }
+        //dd($calendarEvents);
+        $loadJSWithoutDefer = false;
+        return view('admin.home', compact('loginAlert', 'calendarEvents', 'loadJSWithoutDefer'));
+        
     }
     public function showLoginAlert()
     {
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -26,13 +26,64 @@
                     </ol>
                 </nav>
             </div>
+            <div class="row">
+                <div class="col">
+                    <div id="calendar"></div>
+                </div>
+            </div>
             
+
         </div>
     </div>
 </div>
 @endsection
+@push('scripts')
+
+    {{-- Calendar --}}
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.css"/>
+
+@endpush
 
 @section('scripts')
+
+    <script type="text/javascript">
+        {{-- FullCalendar JS --}}
+        var calendar;
+        document.addEventListener("DOMContentLoaded", function(event) { 
+            var calendarEl = document.getElementById('calendar')
+            calendar = new FullCalendar.Calendar(calendarEl,
+            {
+                "headerToolbar":
+                    {
+                        "start": 'title',
+                        "center": '',
+                        "end": "today prev,next dayGridMonth timeGridWeek timeGridDay",
+                        
+                    },
+                "footerToolbar":
+                    {
+                        "center": 'today prev,next dayGridMonth timeGridWeek timeGridDay',
+                        
+                    },
+                "eventLimit":true,
+                "timeZone": "local",
+                "locale":"en",
+                "firstDay": 0,
+                "displayEventTime":true,
+                "selectable":true,
+                "initialView":"dayGridMonth",
+                
+                "events":
+                        JSON.parse('{{ $calendarEvents }}'.replace(/(&quot;)+/g, '"')),
+
+                        
+            },);
+            calendar.render();
+        });
+    </script>
+
     <script type="text/javascript">
         {{-- LOGIN ALERT TIMEOUT --}}
         window.setTimeout(function() {
@@ -41,5 +92,5 @@
             });
         }, 3000);
     </script>
-@endsection
     
+@endsection
