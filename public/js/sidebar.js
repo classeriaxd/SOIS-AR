52c15ed690a0ddb0583afe3ebf170df5bc863aab
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-10-23 01:46:55 +0800

    convert to plain JS

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,11 +1,14 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $('#content').toggleClass('active');
-    });
-    
-    $('.more-button,.body-overlay').on('click', function () {
-        $('#sidebar,.body-overlay').toggleClass('show-nav');
-    });
-    
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+ 
+    document.getElementById('sidebarCollapse').onclick = function()
+    {
+        document.getElementById('sidebar').classList.toggle('active');
+        document.getElementById('content').classList.toggle('active');
+    };
+
+    document.querySelectorAll('.more-button, .body-overlay').onclick = function()
+    {
+        document.querySelectorAll('#sidebar, .body-overlay').classList.toggle('show-nav');
+    }
 });
