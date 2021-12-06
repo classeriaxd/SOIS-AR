3618d68da83abccde6eaaeeccedc4503b55e9dbe
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2021-12-06 16:20:11 +0800

    Update signatoryPage.blade.php

--- resources/views/accomplishmentReports/pdfTemplates/signatoryPage.blade.php
+++ resources/views/accomplishmentReports/pdfTemplates/signatoryPage.blade.php
@@ -16,30 +16,33 @@
 
             {{-- Prepared by: --}}
             <div class="prep-prepared">
+                @if($documentationSignatory->isNotEmpty())
                 <p class="prep-text">Prepared by:</p>
-                @foreach($documentationSignatory as $signatory)
+                    @foreach($documentationSignatory as $signatory)
 
-                    {{-- Signature --}}
-                    @if($signature)
-                        @if($signatory->signature !== NULL)
-                            <img src="{{ public_path('/storage/'. $signatory->signature) }}" class="prep-signature">
+                        {{-- Signature --}}
+                        @if($signature)
+                            @if($signatory->signature !== NULL)
+                                <img src="{{ public_path('/storage/'. $signatory->signature) }}" class="prep-signature">
+                            @else
+                                <p class="prep-signature"></p>
+                            @endif
                         @else
-                            <p class="prep-signature"></p>
+                             <p class="prep-signature"></p>
                         @endif
-                    @else
-                         <p class="prep-signature"></p>
-                    @endif
-                    
-                    {{-- Details --}}
-                    <p class="prep-officer">{{ $signatory->full_name }}</p>
-                    <p class="prep-officer-title">{{ $organization->organization_acronym . ' ' . $signatory->positionTitle->position_title }}</p>
-                @endforeach
+                        
+                        {{-- Details --}}
+                        <p class="prep-officer">{{ $signatory->full_name }}</p>
+                        <p class="prep-officer-title">{{ $organization->organization_acronym . ' ' . $signatory->positionTitle->position_title }}</p>
+                    @endforeach
+                @endif
             </div>
 
             <br>
 
             {{-- Approved by: --}}
             <div class="prep-approved">
+                @if($presidentSignatory->isNotEmpty())
                 <p class="prep-text">Approved by:</p>
                 {{-- Signature --}}
                     @if($signature)
@@ -55,6 +58,7 @@
                 {{-- Details --}}
                 <p class="prep-officer">{{ $presidentSignatory->full_name }}</p>
                 <p class="prep-officer-title">{{ $organization->organization_acronym . ' ' . $presidentSignatory->positionTitle->position_title }}</p>
+                @endif
             </div>
         </div>
     </div>
