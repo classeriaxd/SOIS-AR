1005ef011ad848cddebc841554b305d5bf8c1f75
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2022-03-11 06:27:00 +0800

    change title to ENV APP Name

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'SOIS-AR' }}</title>
+    <title>{{ env('APP_NAME') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
