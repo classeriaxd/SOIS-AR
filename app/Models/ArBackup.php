db3b618460808194d6da4ea85950f40667e84aad
RusselClaveria <78835302+classeriaxd@users.noreply.github.com>
2022-07-21 20:37:59 +0800

    add guarded[] to Model

--- app/Models/ArBackup.php
+++ app/Models/ArBackup.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArBackup extends Model
 {
+    protected $guarded = [];
     protected $primaryKey = 'ar_backup_id';
     protected $table = 'sois_ar_backup';
 }
